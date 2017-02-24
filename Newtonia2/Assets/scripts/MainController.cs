92fd5bbb02790d90ba1f1a8d07f12606cb4f2991
clint-morimoto <clintmor@hawaii.edu>
2017-02-23 14:02:12 -1000

    update on walk animation (speed and loop seems good now)

--- Newtonia2/Assets/scripts/MainController.cs
+++ Newtonia2/Assets/scripts/MainController.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class MainController : MonoBehaviour {
 
+    // Initialize variables that control or modify player movement:
     public float moveSpeed;
     private Rigidbody2D myRigidBody;
 
-	// Use this for initialization
+    // Any object in Unity has a position, which is stored by a Transform data type:
+    public Transform groundCheck;
+
+    // checks where the ground is (relative to a radial distance...):
+    public float groundCheckRadius;
+
+    // Assigns ground to the appropriate layer:
+    public LayerMask whatIsGround;  // note that you CAN assign multiple layers to this variable.
+
+    public bool isGrounded;  // check whether player is on ground (or not)
+    
+    // Use this for initialization
 	void Start () {
         myRigidBody = GetComponent<Rigidbody2D>();
         myRigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;  // YES!
@@ -15,6 +27,10 @@ public class MainController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        // Check to see if player is in contact with whatIsGround layermask:
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
 		if (Input.GetAxisRaw("Horizontal") > 0f)
         {
             myRigidBody.velocity = new Vector3(moveSpeed, myRigidBody.velocity.y, 0f);
@@ -25,5 +41,8 @@ public class MainController : MonoBehaviour {
         {
             myRigidBody.velocity = new Vector3(0f, myRigidBody.velocity.y, 0f);
         }
+
+        // If we wanted a jump action for player, code it here:
+        //if (Input.GetButtonDown(...)) {}
     }
 }
