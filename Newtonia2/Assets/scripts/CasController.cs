fe03faac2d2e1a3c160ce330a1a2f2acb28238aa
clint-morimoto <clintmor@hawaii.edu>
2017-03-14 18:03:53 -1000

    Updated dialogue box style

---
+++ Newtonia2/Assets/scripts/CasController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CasController : MonoBehaviour {
+
+    //  Use this to find the script attached to the main character:
+    private MainController mainPlayer;
+    public GameObject followTarget;
+
+    public float speed;
+
+    public LayerMask playerLayer;
+
+    // Use these next two variables to track the range that determines where CasCopter will STOP following Main.
+    public float mainRange;
+    public bool mainInRange;
+
+	// Use this for initialization
+	void Start () {
+        mainPlayer = FindObjectOfType<MainController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        mainInRange = Physics2D.OverlapCircle(transform.position, mainRange, playerLayer);
+
+        if (!mainInRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, followTarget.transform.position, speed * Time.deltaTime);
+            //transform.position = Vector3.SmoothDamp(transform.position, mainPlayer.transform.position, speed * Time.deltaTime, smoothTime);
+        }
+
+        if (transform.position.x - followTarget.transform.position.x > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        } else if (transform.position.x - followTarget.transform.position.x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(transform.position, mainRange);
+    }
+}
--- Newtonia2/Assets/scripts/MainController.cs
+++ Newtonia2/Assets/scripts/MainController.cs
@@ -6,7 +6,7 @@ public class MainController : MonoBehaviour {
 
     // Initialize variables that control or modify player movement:
     public float moveSpeed;
-    private Rigidbody2D myRigidBody;
+    private Rigidbody2D mainRigidBody;  // assigns the rigidbody2d we made for nMain to this script...
 
     // Any object in Unity has a position, which is stored by a Transform data type:
     public Transform groundCheck;
@@ -18,11 +18,15 @@ public class MainController : MonoBehaviour {
     public LayerMask whatIsGround;  // note that you CAN assign multiple layers to this variable.
 
     public bool isGrounded;  // check whether player is on ground (or not)
+
+    private Animator mainAnim;  // like RigidBody2D class, we must assign our animator to this script.
     
     // Use this for initialization
 	void Start () {
-        myRigidBody = GetComponent<Rigidbody2D>();
-        myRigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;  // YES!
+        mainRigidBody = GetComponent<Rigidbody2D>();
+        mainAnim = GetComponent<Animator>();
+
+        mainRigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;  // YES!
 	}
 	
 	// Update is called once per frame
@@ -33,16 +37,23 @@ public class MainController : MonoBehaviour {
 
 		if (Input.GetAxisRaw("Horizontal") > 0f)
         {
-            myRigidBody.velocity = new Vector3(moveSpeed, myRigidBody.velocity.y, 0f);
+            // make player move right:
+            mainRigidBody.velocity = new Vector3(moveSpeed, mainRigidBody.velocity.y, 0f);
+            // make the sprite face the right way when moving in this direction:
+            transform.localScale = new Vector3(-1f, 1f, 1f);
         } else if (Input.GetAxisRaw("Horizontal") < 0f)
         {
-            myRigidBody.velocity = new Vector3(-moveSpeed, myRigidBody.velocity.y, 0f);
+            mainRigidBody.velocity = new Vector3(-moveSpeed, mainRigidBody.velocity.y, 0f);
+            transform.localScale = new Vector3(1f, 1f, 1f);
         } else
         {
-            myRigidBody.velocity = new Vector3(0f, myRigidBody.velocity.y, 0f);
+            mainRigidBody.velocity = new Vector3(0f, mainRigidBody.velocity.y, 0f);
         }
 
         // If we wanted a jump action for player, code it here:
         //if (Input.GetButtonDown(...)) {}
+
+        mainAnim.SetFloat("speed", Mathf.Abs(mainRigidBody.velocity.x));  // note that we want ANY nonzero value of speed; must convert negative speed to positive speed, so we use Mathf.Abs() to get the abs value...
+        mainAnim.SetBool("grounded", isGrounded);
     }
 }
